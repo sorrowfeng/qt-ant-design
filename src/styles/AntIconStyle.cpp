d78cefecdaf3ca085343ea1d8c36c5bd70807633
sorrowfeng <1399600304@qq.com>
2026-04-29 03:33:55 +0800

    Audit icon interaction states

--- src/styles/AntIconStyle.cpp
+++ src/styles/AntIconStyle.cpp
@@ -470,6 +470,10 @@ const OfficialSvgIcon* officialOutlinedIcon(Ant::IconType type)
         {R"(M518.3 459a8 8 0 00-12.6 0l-112 141.7a7.98 7.98 0 006.3 12.9h73.9V856c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V613.7H624c6.7 0 10.4-7.7 6.3-12.9L518.3 459z)",
          R"(M811.4 366.7C765.6 245.9 648.9 160 512.2 160S258.8 245.8 213 366.6C127.3 389.1 64 467.2 64 560c0 110.5 89.5 200 199.9 200H304c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8h-40.1c-33.7 0-65.4-13.4-89-37.7-23.5-24.2-36-56.8-34.9-90.6.9-26.4 9.9-51.2 26.2-72.1 16.7-21.3 40.1-36.8 66.1-43.7l37.9-9.9 13.9-36.6c8.6-22.8 20.6-44.1 35.7-63.4a245.6 245.6 0 0152.4-49.9c41.1-28.9 89.5-44.2 140-44.2s98.9 15.3 140 44.2c19.9 14 37.5 30.8 52.4 49.9 15.1 19.3 27.1 40.7 35.7 63.4l13.8 36.5 37.8 10C846.1 454.5 884 503.8 884 560c0 33.1-12.9 64.3-36.3 87.7a123.07 123.07 0 01-87.6 36.3H720c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h40.1C870.5 760 960 670.5 960 560c0-92.7-63.1-170.7-148.6-193.3z)"}
     };
+    static const OfficialSvgIcon loading = {
+        "0 0 1024 1024",
+        {R"(M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z)", nullptr}
+    };
 
     switch (type)
     {
@@ -489,8 +493,77 @@ const OfficialSvgIcon* officialOutlinedIcon(Ant::IconType type)
     case Ant::IconType::Edit: return &edit;
     case Ant::IconType::Delete: return &remove;
     case Ant::IconType::CloudUpload: return &upload;
+    case Ant::IconType::Loading: return &loading;
     default: return nullptr;
     }
+}
+
+const OfficialSvgIcon* officialFilledIcon(Ant::IconType type)
+{
+    static const OfficialSvgIcon setting = {
+        "64 64 896 896",
+        {R"(M512.5 390.6c-29.9 0-57.9 11.6-79.1 32.8-21.1 21.2-32.8 49.2-32.8 79.1 0 29.9 11.7 57.9 32.8 79.1 21.2 21.1 49.2 32.8 79.1 32.8 29.9 0 57.9-11.7 79.1-32.8 21.1-21.2 32.8-49.2 32.8-79.1 0-29.9-11.7-57.9-32.8-79.1a110.96 110.96 0 00-79.1-32.8zm412.3 235.5l-65.4-55.9c3.1-19 4.7-38.4 4.7-57.7s-1.6-38.8-4.7-57.7l65.4-55.9a32.03 32.03 0 009.3-35.2l-.9-2.6a442.5 442.5 0 00-79.6-137.7l-1.8-2.1a32.12 32.12 0 00-35.1-9.5l-81.2 28.9c-30-24.6-63.4-44-99.6-57.5l-15.7-84.9a32.05 32.05 0 00-25.8-25.7l-2.7-.5c-52-9.4-106.8-9.4-158.8 0l-2.7.5a32.05 32.05 0 00-25.8 25.7l-15.8 85.3a353.44 353.44 0 00-98.9 57.3l-81.8-29.1a32 32 0 00-35.1 9.5l-1.8 2.1a445.93 445.93 0 00-79.6 137.7l-.9 2.6c-4.5 12.5-.8 26.5 9.3 35.2l66.2 56.5c-3.1 18.8-4.6 38-4.6 57 0 19.2 1.5 38.4 4.6 57l-66 56.5a32.03 32.03 0 00-9.3 35.2l.9 2.6c18.1 50.3 44.8 96.8 79.6 137.7l1.8 2.1a32.12 32.12 0 0035.1 9.5l81.8-29.1c29.8 24.5 63 43.9 98.9 57.3l15.8 85.3a32.05 32.05 0 0025.8 25.7l2.7.5a448.27 448.27 0 00158.8 0l2.7-.5a32.05 32.05 0 0025.8-25.7l15.7-84.9c36.2-13.6 69.6-32.9 99.6-57.5l81.2 28.9a32 32 0 0035.1-9.5l1.8-2.1c34.8-41.1 61.5-87.4 79.6-137.7l.9-2.6c4.3-12.4.6-26.3-9.5-35zm-412.3 52.2c-97.1 0-175.8-78.7-175.8-175.8s78.7-175.8 175.8-175.8 175.8 78.7 175.8 175.8-78.7 175.8-175.8 175.8z)", nullptr}
+    };
+
+    switch (type)
+    {
+    case Ant::IconType::Setting: return &setting;
+    default: return nullptr;
+    }
+}
+
+struct OfficialTwoToneIcon
+{
+    const char* viewBox = "64 64 896 896";
+    const char* paths[3] = {nullptr, nullptr, nullptr};
+    int colors[3] = {0, 1, 0};
+};
+
+const OfficialTwoToneIcon* officialTwoToneIcon(Ant::IconType type)
+{
+    static const OfficialTwoToneIcon checkCircle = {
+        "64 64 896 896",
+        {R"(M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z)",
+         R"(M512 140c-205.4 0-372 166.6-372 372s166.6 372 372 372 372-166.6 372-372-166.6-372-372-372zm193.4 225.7l-210.6 292a31.8 31.8 0 01-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.3 0 19.9 5 25.9 13.3l71.2 98.8 157.2-218c6-8.4 15.7-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.4 12.7z)",
+         R"(M699 353h-46.9c-10.2 0-19.9 4.9-25.9 13.3L469 584.3l-71.2-98.8c-6-8.3-15.6-13.3-25.9-13.3H325c-6.5 0-10.3 7.4-6.5 12.7l124.6 172.8a31.8 31.8 0 0051.7 0l210.6-292c3.9-5.3.1-12.7-6.4-12.7z)"},
+        {0, 1, 0}
+    };
+    static const OfficialTwoToneIcon heart = {
+        "64 64 896 896",
+        {R"(M923 283.6a260.04 260.04 0 00-56.9-82.8 264.4 264.4 0 00-84-55.5A265.34 265.34 0 00679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 00-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9zM512 814.8S156 586.7 156 385.5C156 283.6 240.3 201 344.3 201c73.1 0 136.5 40.8 167.7 100.4C543.2 241.8 606.6 201 679.7 201c104 0 188.3 82.6 188.3 184.5 0 201.2-356 429.3-356 429.3z)",
+         R"(M679.7 201c-73.1 0-136.5 40.8-167.7 100.4C480.8 241.8 417.4 201 344.3 201c-104 0-188.3 82.6-188.3 184.5 0 201.2 356 429.3 356 429.3s356-228.1 356-429.3C868 283.6 783.7 201 679.7 201z)",
+         nullptr},
+        {0, 1, 0}
+    };
+
+    switch (type)
+    {
+    case Ant::IconType::CheckCircle: return &checkCircle;
+    case Ant::IconType::Heart: return &heart;
+    default: return nullptr;
+    }
+}
+
+QColor officialTwoToneSecondaryColor(const QColor& primary)
+{
+    const QString hex = primary.name(QColor::HexRgb).toLower();
+    if (hex == QStringLiteral("#52c41a"))
+    {
+        return QColor("#f6ffed");
+    }
+    if (hex == QStringLiteral("#49aa19"))
+    {
+        return QColor("#e4ebdd");
+    }
+    if (hex == QStringLiteral("#eb2f96"))
+    {
+        return QColor("#fff0f6");
+    }
+    if (hex == QStringLiteral("#1677ff") || hex == QStringLiteral("#1668dc"))
+    {
+        return QColor("#e6f4ff");
+    }
+    return AntPalette::tint(primary, 0.92);
 }
 
 bool drawOfficialOutlinedIcon(Ant::IconType type, const QRectF& iconRect, const QColor& color, QPainter* painter)
@@ -511,6 +584,73 @@ bool drawOfficialOutlinedIcon(Ant::IconType type, const QRectF& iconRect, const 
         }
         svg += QStringLiteral("<path fill=\"");
         svg += fill;
+        svg += QStringLiteral("\" d=\"");
+        svg += QString::fromLatin1(pathData);
+        svg += QStringLiteral("\"/>");
+    }
+    svg += QStringLiteral("</svg>");
+
+    QSvgRenderer renderer(svg.toUtf8());
+    if (!renderer.isValid())
+    {
+        return false;
+    }
+    renderer.render(painter, iconRect);
+    return true;
+}
+
+bool drawOfficialFilledIcon(Ant::IconType type, const QRectF& iconRect, const QColor& color, QPainter* painter)
+{
+    const auto* data = officialFilledIcon(type);
+    if (!data || !painter)
+    {
+        return false;
+    }
+
+    QString svg = QStringLiteral("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"%1\">").arg(QString::fromLatin1(data->viewBox));
+    const QString fill = color.name(QColor::HexRgb);
+    for (const char* pathData : data->paths)
+    {
+        if (!pathData)
+        {
+            continue;
+        }
+        svg += QStringLiteral("<path fill=\"");
+        svg += fill;
+        svg += QStringLiteral("\" d=\"");
+        svg += QString::fromLatin1(pathData);
+        svg += QStringLiteral("\"/>");
+    }
+    svg += QStringLiteral("</svg>");
+
+    QSvgRenderer renderer(svg.toUtf8());
+    if (!renderer.isValid())
+    {
+        return false;
+    }
+    renderer.render(painter, iconRect);
+    return true;
+}
+
+bool drawOfficialTwoToneIcon(Ant::IconType type, const QRectF& iconRect, const QColor& primaryColor, const QColor& secondaryColor, QPainter* painter)
+{
+    const auto* data = officialTwoToneIcon(type);
+    if (!data || !painter)
+    {
+        return false;
+    }
+
+    QString svg = QStringLiteral("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"%1\">").arg(QString::fromLatin1(data->viewBox));
+    const QString fills[2] = {primaryColor.name(QColor::HexRgb), secondaryColor.name(QColor::HexRgb)};
+    for (int i = 0; i < 3; ++i)
+    {
+        const char* pathData = data->paths[i];
+        if (!pathData)
+        {
+            continue;
+        }
+        svg += QStringLiteral("<path fill=\"");
+        svg += fills[data->colors[i]];
         svg += QStringLiteral("\" d=\"");
         svg += QString::fromLatin1(pathData);
         svg += QStringLiteral("\"/>");
@@ -642,6 +782,18 @@ void AntIconStyle::drawIcon(const QStyleOption* option, QPainter* painter, const
         painter->restore();
         return;
     }
+    if (!icon->hasCustomPath() && iconTheme == Ant::IconTheme::Filled &&
+        drawOfficialFilledIcon(iconType, iconRect, primaryColor, painter))
+    {
+        painter->restore();
+        return;
+    }
+    if (!icon->hasCustomPath() && iconTheme == Ant::IconTheme::TwoTone &&
+        drawOfficialTwoToneIcon(iconType, iconRect, primaryColor, officialTwoToneSecondaryColor(primaryColor), painter))
+    {
+        painter->restore();
+        return;
+    }
 
     QColor secondaryColor;
     if (!enabled)
@@ -651,7 +803,7 @@ void AntIconStyle::drawIcon(const QStyleOption* option, QPainter* painter, const
     else if (iconTheme == Ant::IconTheme::TwoTone)
     {
         const QColor tone = icon->twoToneColor().isValid() ? icon->twoToneColor() : token.colorPrimary;
-        secondaryColor = AntPalette::tint(tone, 0.72);
+        secondaryColor = officialTwoToneSecondaryColor(tone);
     }
     else if (iconTheme == Ant::IconTheme::Filled)
     {
