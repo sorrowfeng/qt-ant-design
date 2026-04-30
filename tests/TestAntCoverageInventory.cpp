0e6d796e5b95d9185302776eb5bfc080eddd3233
sorrowfeng <1399600304@qq.com>
2026-04-30 22:35:14 +0800

    Add widget test coverage inventory guard

---
+++ tests/TestAntCoverageInventory.cpp
@@ -0,0 +1,84 @@
+#include <QDir>
+#include <QFile>
+#include <QRegularExpression>
+#include <QSet>
+#include <QStringList>
+#include <QTest>
+
+#ifndef ANT_WIDGETS_DIR
+#error "ANT_WIDGETS_DIR must be defined by tests/CMakeLists.txt"
+#endif
+
+#ifndef ANT_TESTS_DIR
+#error "ANT_TESTS_DIR must be defined by tests/CMakeLists.txt"
+#endif
+
+class TestAntCoverageInventory : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void publicWidgetHeadersAreInLifecycleAndMetaTests();
+};
+
+namespace
+{
+QString readTextFile(const QString& path)
+{
+    QFile file(path);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        return {};
+    }
+    return QString::fromUtf8(file.readAll());
+}
+
+QSet<QString> publicWidgetHeaders()
+{
+    QSet<QString> headers;
+    const QDir widgetsDir(QStringLiteral(ANT_WIDGETS_DIR));
+    const QFileInfoList files = widgetsDir.entryInfoList({QStringLiteral("Ant*.h")}, QDir::Files, QDir::Name);
+    for (const QFileInfo& file : files)
+    {
+        const QString baseName = file.completeBaseName();
+        if (baseName == QStringLiteral("AntSelectPopup"))
+        {
+            continue;
+        }
+        headers.insert(baseName);
+    }
+    return headers;
+}
+
+QSet<QString> includedWidgetHeaders(const QString& fileName)
+{
+    const QString text = readTextFile(QStringLiteral(ANT_TESTS_DIR) + QLatin1Char('/') + fileName);
+    QSet<QString> headers;
+
+    QRegularExpression includePattern(QStringLiteral("[#]include\\s+\"widgets/(Ant[^\"]+)\\.h\""));
+    QRegularExpressionMatchIterator it = includePattern.globalMatch(text);
+    while (it.hasNext())
+    {
+        headers.insert(it.next().captured(1));
+    }
+    return headers;
+}
+} // namespace
+
+void TestAntCoverageInventory::publicWidgetHeadersAreInLifecycleAndMetaTests()
+{
+    const QSet<QString> publicHeaders = publicWidgetHeaders();
+    QVERIFY(!publicHeaders.isEmpty());
+
+    const QSet<QString> objectTreeHeaders = includedWidgetHeaders(QStringLiteral("TestAntObjectTree.cpp"));
+    const QSet<QString> metaPropertyHeaders = includedWidgetHeaders(QStringLiteral("TestAntMetaProperties.cpp"));
+
+    const QStringList missingObjectTree = (publicHeaders - objectTreeHeaders).values();
+    const QStringList missingMetaProperties = (publicHeaders - metaPropertyHeaders).values();
+
+    QCOMPARE(missingObjectTree, QStringList());
+    QCOMPARE(missingMetaProperties, QStringList());
+}
+
+QTEST_MAIN(TestAntCoverageInventory)
+#include "TestAntCoverageInventory.moc"
